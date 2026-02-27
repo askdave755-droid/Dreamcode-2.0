496686d2fc0899b6142b14f32e0f92469e24a302
askdave755-droid <askdave755@gmail.com>
2026-02-27 06:56:25 -0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, HTTPException, Depends, BackgroundTasks, Deploy fix
+# Deploy fix
+from fastapi import FastAPI, HTTPException, Depends, BackgroundTasks
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from datetime import datetime
